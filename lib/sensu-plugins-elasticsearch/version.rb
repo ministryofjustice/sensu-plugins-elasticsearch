61dd9c7ec978d0d255b1a1dbb3770f6eb30590e4
Lukasz Raczylo <lukasz@raczylo.com>
2016-03-30 12:11:55 +0100

    conflict solved

--- lib/sensu-plugins-elasticsearch/version.rb
+++ lib/sensu-plugins-elasticsearch/version.rb
@@ -1,8 +1,13 @@
 module SensuPluginsElasticsearch
   module Version
     MAJOR = 0
+<<<<<<< HEAD
     MINOR = 4
     PATCH = 3
+=======
+    MINOR = 2
+    PATCH = 0
+>>>>>>> f1764c4... version bump to 0.2.0
 
     VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
